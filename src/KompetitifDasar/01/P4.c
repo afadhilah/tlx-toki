f8d60227d43628be54d7be45e700a2f7f25959c7
afadhilah <afadhilah2004@gmail.com>
2025-01-05 11:09:50 +0800

    Add matrix transformation program in P4.c to format input string into a zigzag matrix

---
+++ src/KompetitifDasar/01/P4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main() {
+    char input[256]; // Maksimal panjang input adalah 255 karakter
+    scanf("%s", input);
+
+    int length = strlen(input); // Panjang string input
+    int N = (int)ceil(sqrt(length)); // Ukuran sisi matriks terkecil
+
+    char matrix[N][N]; // Matriks hasil
+    int index = 0;     // Indeks untuk membaca karakter input
+
+    // Isi matriks dengan karakter dari input
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (index < length) {
+                matrix[i][j] = input[index++];
+            } else {
+                matrix[i][j] = '.'; // Isi sisa dengan '.'
+            }
+        }
+    }
+
+    // Cetak matriks dengan baris genap terbalik
+    for (int i = 0; i < N; i++) {
+        if (i % 2 == 0) { // Baris ganjil (indeks genap)
+            for (int j = 0; j < N; j++) {
+                printf("%c", matrix[i][j]);
+            }
+        } else { // Baris genap (indeks ganjil)
+            for (int j = N - 1; j >= 0; j--) {
+                printf("%c", matrix[i][j]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
